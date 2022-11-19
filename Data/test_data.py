7fcda4b09720fe7848bd15bffd3bd5286955e2c2
Reanold Piatrenka <ronny812@gmail.com>
2022-11-19 22:57:24 +0200

    Add free API list

--- Data/test_data.py
+++ Data/test_data.py
@@ -1,5 +1,6 @@
 
 # this function returns one random value from defined set at a time
+# free API for testing purposes = https://apipheny.io/free-api/#apis-without-key
 
 def violations():
     words_set = {'empty', 'words', 'set'}
