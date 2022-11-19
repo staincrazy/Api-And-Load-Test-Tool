c5d370f082bbf44411d7ef24690729fee174c630
Reanold Piatrenka <ronny812@gmail.com>
2022-11-19 22:30:43 +0200

    Add sample get request test

--- Adaptors/sync_adaptors.py
+++ Adaptors/sync_adaptors.py
@@ -4,10 +4,15 @@ import requests
 class SyncRequestsAdapter:
     @staticmethod
     def post_request(endpoint_url, data, api_key):
-
         headers = {'af-api-key': api_key}
         response = requests.post(url=endpoint_url, data=data, headers=headers)
 
         return response
 
+    @staticmethod
+    def get_request(endpoint_url, data=None):
+        if data is None:
+            data = data
+        response = requests.get(url=endpoint_url, data=data)
 
+        return response
--- Tests/test_free_endpoints.py
+++ Tests/test_free_endpoints.py
@@ -1,14 +1,18 @@
+from Data.test_objects import CatFactsAsyncAPi
 from Utils.base_test_case import BaseTestCase
-
+from Adaptors.sync_adaptors import SyncRequestsAdapter as Request
 
 class TestEndpoints(BaseTestCase):
     def setUp(self):
         super(TestEndpoints, self).setUp()
-        self.endpoint_url = ""
+        self.endpoint_url = CatFactsAsyncAPi.get_randomCatFact().endpoint
 
     def tearDown(self):
         super(TestEndpoints, self).tearDown()
 
 
     def test_cat_facts(self):
-        pass
+
+        func = Request.get_request(self.endpoint_url)
+
+        assert func.status_code == 200
